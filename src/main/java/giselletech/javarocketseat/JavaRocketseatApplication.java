ea5d1442cc010ff1576b5960695643f65fa601fc
Giselle Ferreira <giselle.fs@gmail.com>
2023-10-10 16:27:01 -0300

    Initial Commit

---
+++ src/main/java/giselletech/javarocketseat/JavaRocketseatApplication.java
@@ -0,0 +1,15 @@
+package giselletech.javarocketseat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaRocketseatApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(JavaRocketseatApplication.class, args);
+		System.out.println("rodando");
+	}
+
+}
---
+++ src/test/java/giselletech/javarocketseat/JavaRocketseatApplicationTests.java
@@ -0,0 +1,13 @@
+package giselletech.javarocketseat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaRocketseatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
